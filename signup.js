623833fdfd95073932a4125136e86caf16074604
abthe7 <144072454+abthe7@users.noreply.github.com>
2026-08-14 11:42:06 +0100

    Delete signup.js

--- signup.js
+++
@@ -1,6 +0,0 @@
-const passwordInput = document.getElementById("password");
-const passwordToggle = document.getElementById("show-password");
-
-passwordToggle.addEventListener("change", () => {
-    passwordInput.type = passwordToggle.checked ? "text" : "password";
-});
